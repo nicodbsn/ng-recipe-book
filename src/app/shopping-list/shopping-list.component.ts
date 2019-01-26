96d5792b88f56634d5b51407802568086b75ea14
nicodbsn <dubuisson53@gmail.com>
2019-01-26 15:12:15 +0100

    Add ingredients to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  private onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 }
