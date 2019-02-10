37668f708cfefb17ebf6ea8140ed8804f5fee548
nicodbsn <dubuisson53@gmail.com>
2019-02-10 17:07:05 +0100

    Display ingredients in the recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,28 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
-        new Recipe('Another recipe', 'this is another test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+        new Recipe(
+            'Tasty Schnitzel', 
+            'A super-tasty Schnitzel - just awesome !', 
+            'https://static.essen-und-trinken.de/bilder/e9/b1/7321/galleryimage/ed956d4fa02f282fabdb282aa6bc6447.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Big Fat Burger',
+            'What else you need to say', 
+            'https://images1.laweekly.com/imager/u/original/8690577/large-fatburger-burger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1),
+                new Ingredient('Cheddar', 2)
+            ])
     ];
 
     getRecipes() {
